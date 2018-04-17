fbedb589a03f7b752f09a4ff025f05f6bab49f86
RaviKN12 <38257519+RaviKN12@users.noreply.github.com>
2018-04-17 23:23:46 +0530

    Add files via upload

---
+++ Code/Code/Question1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class Question1
+    {
+        static void Main(string[] args)
+        {
+            Question1 q1 = new Question1();
+            Question3 q3 = new Question3();
+
+            q1.doesFileExist("C:\\testfile.txt");
+            q1.readDictionaryFile("C:\\testfile.txt");
+            q3.FindNthSmallestNum(3);
+        }
+
+        public void doesFileExist(string path)
+        {
+
+            try
+            {
+                if (!File.Exists(path))
+                { throw new FileNotFoundException(); }
+                else if (File.Exists(path))
+                {
+                    Console.WriteLine("File Exists");
+                }
+
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void readDictionaryFile(string path)
+        {
+            string contents = File.ReadAllText(path);
+            string[] arr = contents.Split(new Char[] { '-', ',', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            int arrSize = arr.Length;
+            for (int i = 0; i < arrSize; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
---
+++ Code/Code/Question3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code
+{
+    class Question3
+    {
+
+        public void FindNthSmallestNum(int n)
+        {
+            // generating random numbers and storing in an array
+            int[] randomNumbers = new int[500];
+
+            for (int x = 0; x < 500; x++)
+            {
+                randomNumbers[x] = (new Random()).Next();
+            }
+
+            // soritng the array
+            for (int i = 1; i <= 500; i++)
+            {
+                for (int j = 1; j <= 499; j++)
+                {
+                    if (randomNumbers[j] > randomNumbers[j + 1])
+                    {
+                        int temp = randomNumbers[j];
+                        randomNumbers[j] = randomNumbers[j + 1];
+                        randomNumbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine(n + "th smallest number is: " + randomNumbers[n - 1]);
+
+
+        }
+    }
+}
